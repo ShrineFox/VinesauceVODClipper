0bacc9632ec8c75c0b8b160b384e7d91bc3bf461
ShrineFox <ShrineFox@users.noreply.github.com>
2025-01-07 16:49:47 -0500

    Display items from list in DataGrid

---
+++ DataGridItem.cs
@@ -0,0 +1,11 @@
+﻿namespace VinesauceVODClipper
+{
+    public class DataGridItem
+    {
+        public string Title { get; set; } = "";
+        public string Path { get; set; } = "";
+        public TimeSpan StartTime { get; set; } = new TimeSpan(hours: 0, minutes: 0, seconds: 0);
+        public TimeSpan EndTime { get; set; } = new TimeSpan(hours: 0, minutes: 0, seconds: 0);
+
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,17 +8,28 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ShrineFox.IO;
+using System.IO;
 
 namespace VinesauceVODClipper
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : System.Windows.Window
     {
+        string ffmpegPath = "";
+        private readonly ViewModel viewModel;
+
         public MainWindow()
         {
             InitializeComponent();
+            this.viewModel = new ViewModel()
+            {
+                DataGridItems = new List<DataGridItem>() { new DataGridItem() {  Title = "Test lol" } }
+            };
+            this.DataContext = this.viewModel;
+            ffmpegPath = System.IO.Path.Combine(Exe.Directory(), "Dependencies//ffmpeg.exe");
         }
     }
 }
