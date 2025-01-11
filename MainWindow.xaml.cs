132886e9264a4ac5c49e04f1ca7697108af760da
ShrineFox <ShrineFox@users.noreply.github.com>
2025-01-10 22:19:26 -0500

    Import video list from txt and update form rows

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Shapes;
 using ShrineFox.IO;
 using System.IO;
 using VinesauceVODClipper.Controls;
+using System.Windows.Forms;
+using System.Collections.ObjectModel;
 
 namespace VinesauceVODClipper
 {
@@ -28,7 +30,7 @@ namespace VinesauceVODClipper
             
             this.viewModel = new ViewModel()
             {
-                DataGridItems = new List<DataGridItem>() { new DataGridItem() {  Title = "Test lol", Path = @"C:\Path\To\FullVOD.mp4" } },
+                DataGridItems = new ObservableCollection<DataGridItem>() { new DataGridItem() {  Title = "Test lol", Path = @"C:\Path\To\FullVOD.mp4" } },
             };
             this.DataContext = this.viewModel;
             ffmpegPath = System.IO.Path.Combine(Exe.Directory(), "Dependencies//ffmpeg.exe");
@@ -64,5 +66,80 @@ namespace VinesauceVODClipper
             }
         }
 
+        private void ImportList_Click(object sender, EventArgs e)
+        {
+            var selectedFiles = WinFormsDialogs.SelectFile("Choose .txt with Timestamps", false, new string[] { "Text Document (.txt)" });
+            if (selectedFiles.Count > 0 && File.Exists(selectedFiles.First()))
+            {
+                ImportTxtFile(selectedFiles.First());
+            }
+        }
+
+        private string TimestampStringToHourFormat(string timeStamp)
+        {
+            if (timeStamp.Split(':').Length == 2)
+            {
+                // Add hours to timestamp if missing
+                return "00:" + timeStamp;
+            }
+            return timeStamp;
+        }
+
+        private void ImportTxtFile(string txtPath)
+        {
+            // Exit early if the user doesn't want to overwrite video list
+            if (viewModel.DataGridItems.Count > 0 && !WinFormsDialogs.ShowMessageBox(
+                "Overwrite Video List Form", "Changing the input text file will reset the current video list. " +
+                "Do you want to continue?", MessageBoxButtons.YesNo))
+            {
+                return;
+            }
+
+            var txtLines = File.ReadAllLines(txtPath);
+            IList<DataGridItem> videoList = new List<DataGridItem>();
+
+            // Create new video list
+            for (int i = 0; i < txtLines.Length; i++)
+            {
+                var splitLine = txtLines[i].Trim().Split(' ');
+
+                if (splitLine.Length < 3)
+                    Output.Log($"Syntax error on line {i + 1}: Missing timestamp?\n\t{txtLines[i]}", ConsoleColor.Yellow);
+                else
+                {
+                    DataGridItem video = new DataGridItem();
+
+                    // Attempt to get timestamps from .txt line
+                    string startTime = TimestampStringToHourFormat(splitLine[splitLine.Length - 2]);
+                    try
+                    {
+                        video.StartTime = TimeSpan.Parse(startTime);
+                    }
+                    catch
+                    {
+                        Output.Log($"Syntax error on line {i + 1}: Invalid Start Time timestamp: \"{startTime}\"" +
+                            $"\nFormat must be hh:mm:ss", ConsoleColor.Yellow);
+                    }
+                    string endTime = TimestampStringToHourFormat(splitLine.Last());
+                    try
+                    {
+                        video.EndTime = TimeSpan.Parse(endTime);
+                    }
+                    catch
+                    {
+                        Output.Log($"Syntax error on line {i + 1}: Invalid End Time timestamp: \"{endTime}\"" +
+                            $"\nFormat must be hh:mm:ss", ConsoleColor.Yellow);
+                    }
+
+                    // Join all strings except the last two timestamps to create Title
+                    video.Title = string.Join(" ", splitLine, 0, splitLine.Length - 2);
+                    videoList.Add(video);
+                }
+            }
+
+            viewModel.DataGridItems.Clear();
+            viewModel.DataGridItems = new ObservableCollection<DataGridItem>(videoList.Copy());
+            Output.Log($"Done reading {videoList.Count} entries from file:\n\t{txtPath}", ConsoleColor.Green);
+        }
     }
 }
--- ViewModel.cs
+++ ViewModel.cs
@@ -1,13 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace VinesauceVODClipper
 {
-    internal class ViewModel
+    internal class ViewModel : INotifyPropertyChanged
     {
-        public IList<DataGridItem> DataGridItems { get; set; }
+        private ObservableCollection<DataGridItem> _dataGridItems;
+        public ObservableCollection<DataGridItem> DataGridItems
+        {
+            get => _dataGridItems;
+            set
+            {
+                _dataGridItems = value;
+                OnPropertyChanged(nameof(DataGridItems));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
